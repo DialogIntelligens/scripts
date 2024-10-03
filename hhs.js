1ea942f6f8ef7843e433d794ee2ebb751c41b825
DialogIntelligens <145685451+DialogIntelligens@users.noreply.github.com>
2024-10-03 12:13:34 +0200

    Update hhs.js

--- hhs.js
+++ hhs.js
@@ -107,9 +107,9 @@ document.addEventListener('DOMContentLoaded', function() {
       titleLogoG: "https://dialogintelligens.dk/wp-content/uploads/2024/09/WhiteMessageIcon.png",
       headerLogoG: "https://dialogintelligens.dk/wp-content/uploads/2024/09/Logo.png",
       themeColor: "#2a803c",
-      headerTitleG: "Skadedyrshops Virtuelle Assistent",
+      headerTitleG: "SkadedyrShops Virtuelle Assistent",
       headerSubtitleG: "Du skriver med en AI. Ved at bruge denne chatbot accepterer du, at samtalen kan gemmes og behandles. Læs mere i vores privatlivspolitik.",
-      titleG: "Skadedyrshop",
+      titleG: "SkadedyrShop",
       firstMessage: "Hej😊 Hvad kan jeg hjælpe dig med?🐝",
       isTabletView: window.innerWidth < 1000 && window.innerWidth > 800,
       isPhoneView: window.innerWidth < 800
