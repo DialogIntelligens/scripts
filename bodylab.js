68e109044e068fe720ae011f2926f8242f6c07c2
DialogIntelligens <145685451+DialogIntelligens@users.noreply.github.com>
2024-10-24 19:13:31 +0200

    Update bodylab.js

--- bodylab.js
+++ bodylab.js
@@ -125,7 +125,7 @@ onDOMReady(function() {
     var isTabletView = window.innerWidth < 1000 && window.innerWidth > 800;
     var isPhoneView = window.innerWidth < 800;
 
-if (window.innerWidth >= 2000) {
+if (window.innerWidth >= 2600) {
   // Fixed size for monitor screens
   iframe.style.width = '500px';
   iframe.style.height = '700px';
