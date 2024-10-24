0daae977e91d2b76373d78603926706298251479
DialogIntelligens <145685451+DialogIntelligens@users.noreply.github.com>
2024-10-24 19:06:40 +0200

    minotor view bodylab

--- bodylab.js
+++ bodylab.js
@@ -125,13 +125,18 @@ onDOMReady(function() {
     var isTabletView = window.innerWidth < 1000 && window.innerWidth > 800;
     var isPhoneView = window.innerWidth < 800;
 
-    if (isIframeEnlarged) {
-      iframe.style.width = 'calc(2 * 45vh + 6vw)';
-      iframe.style.height = '90vh';
-    } else {
-      iframe.style.width = window.innerWidth < 1000 ? '95vw' : 'calc(45vh + 6vw)';
-      iframe.style.height = '90vh';
-    }
+if (window.innerWidth >= 2000) {
+  // Fixed size for monitor screens
+  iframe.style.width = '500px';
+  iframe.style.height = '700px';
+} else if (isIframeEnlarged) {
+  iframe.style.width = 'calc(2 * 45vh + 6vw)';
+  iframe.style.height = '90vh';
+} else {
+  iframe.style.width = window.innerWidth < 1000 ? '95vw' : 'calc(45vh + 6vw)';
+  iframe.style.height = '90vh';
+}
+
 
     iframe.style.position = 'fixed';
     iframe.style.left = window.innerWidth < 1000 ? '50%' : 'auto';
