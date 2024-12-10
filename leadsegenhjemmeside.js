fb15fb18b13ce77e495337f372f009632d925924
DialogIntelligens <145685451+DialogIntelligens@users.noreply.github.com>
2024-12-10 14:53:06 +0100

    Update leadsegenhjemmeside.js

--- leadsegenhjemmeside.js
+++ leadsegenhjemmeside.js
@@ -1,4 +1,14 @@
 // Wait until the DOM is fully loaded
+
+<!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-KV5ZHMDX');</script>
+<!-- End Google Tag Manager -->
+
+
 document.addEventListener('DOMContentLoaded', function() {
   // Inject CSS into the head
   var css = `
@@ -283,3 +293,4 @@ document.addEventListener('DOMContentLoaded', function() {
   // Start the speech balloon management when the page loads
   manageSpeechBalloon();
 });
+
