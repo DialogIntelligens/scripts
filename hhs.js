885d6a9a0a1c44a8300b0ea788a35e32ddb85b6e
DialogIntelligens <145685451+DialogIntelligens@users.noreply.github.com>
2024-12-10 16:15:21 +0100

    Update hhs.js

--- hhs.js
+++ hhs.js
@@ -1,6 +1,18 @@
 // Wait until the DOM is fully loaded
-document.addEventListener('DOMContentLoaded', function() {
-  // Inject CSS into the head
+document.addEventListener('DOMContentLoaded', function () {
+  // Dynamically inject Google Tag Manager
+  (function (w, d, s, l, i) {
+    w[l] = w[l] || [];
+    w[l].push({ 'gtm.start': new Date().getTime(), event: 'gtm.js' });
+    var f = d.getElementsByTagName(s)[0],
+      j = d.createElement(s),
+      dl = l != 'dataLayer' ? '&l=' + l : '';
+    j.async = true;
+    j.src = 'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
+    f.parentNode.insertBefore(j, f);
+  })(window, document, 'script', 'dataLayer', 'GTM-N3MZDZ65');
+
+  
   var css = `
     /* Container for chat button and speech balloon */
     #chat-container {
