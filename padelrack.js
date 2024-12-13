8f7a3b8204e79752de626570361344f70fc10dca
DialogIntelligens <145685451+DialogIntelligens@users.noreply.github.com>
2024-12-13 11:42:30 +0100

    Update padelrack.js

--- padelrack.js
+++ padelrack.js
@@ -69,7 +69,7 @@ document.addEventListener('DOMContentLoaded', function() {
     <div id="chat-container">
       <!-- Chat Button -->
       <button id="chat-button">
-        <img src="http://dialogintelligens.dk/wp-content/uploads/2024/11/VeganActiveMessageLogo.png" alt="Chat with us">
+        <img src="http://dialogintelligens.dk/wp-content/uploads/2024/12/padelRackMessageLogo.png" alt="Chat with us">
       </button>
 
       <!-- Speech Balloon GIF with Close Button -->
