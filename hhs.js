59a3a967db6c3f3d2ebf4b0234945ef6fb816469
DialogIntelligens <145685451+DialogIntelligens@users.noreply.github.com>
2024-12-23 20:14:30 +0100

    Update hhs.js

--- hhs.js
+++ hhs.js
@@ -28,7 +28,7 @@ document.addEventListener('DOMContentLoaded', function () {
       width: 60px;
       height: 60px;
       top: 0px;
-      left: 6px;
+      left: 8px;
       fill: var(--pulse-background);
       fill: #13B981;
       z-index: -1;
