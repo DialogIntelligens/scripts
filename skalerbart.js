5478f66e8b87c1fcad5d9231651857279a043cde
DialogIntelligens <145685451+DialogIntelligens@users.noreply.github.com>
2025-01-28 09:57:05 +0100

    Update skalerbart.js

--- skalerbart.js
+++ skalerbart.js
@@ -239,7 +239,7 @@ document.addEventListener('DOMContentLoaded', function() {
     <iframe
       id="chat-iframe"
       src="https://skalerbartprodukt.onrender.com"
-      style="display: none; position: fixed; bottom: 3vh; right: 2vw; width: 50vh; height: 90vh; border: none; z-index: 40000;">
+      style="display: none; position: fixed; bottom: 1vh; right: 1vw; width: 50vh; height: 90vh; border: none; z-index: 40000;">
     </iframe>
   `;
   document.body.insertAdjacentHTML('beforeend', chatbotHTML);
