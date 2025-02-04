a1447fa9941a3c67e29e693fdd93d7a33bd52da0
DialogIntelligens <145685451+DialogIntelligens@users.noreply.github.com>
2025-02-04 20:51:57 +0100

    Update skalerbart.js

--- skalerbart.js
+++ skalerbart.js
@@ -406,7 +406,7 @@ document.addEventListener('DOMContentLoaded', function() {
       var userHasVisited = getCookie("userHasVisited");
       if (!userHasVisited) {
         setCookie("userHasVisited", "true", 1, ".yourdomain.com");
-        messageBox.innerHTML = `Har du brug for hjælp? <span id="funny-smiley">😄</span>`;
+        messageBox.innerHTML = `Har du brug for hjælp? Eller skal oscar bare prutte? <span id="funny-smiley">😄</span>`;
       } else {
         messageBox.innerHTML = `Velkommen tilbage! Har du brug for hjælp? <span id="funny-smiley">😄</span>`;
       }
