580e856f211faf21757dae1c2d777d246825be93
Chatbot Dashboard <dashboard@yourdomain.com>
2025-03-20 10:17:06 +0100

    Update chatbot variables from Chatbot.js

--- Noroff.js
+++ Noroff.js
@@ -396,7 +396,7 @@ document.addEventListener('DOMContentLoaded', function() {
       var userHasVisited = getCookie("userHasVisited");
       if (!userHasVisited) {
         setCookie("userHasVisited", "true", 1, ".yourdomain.com");
-        messageBox.innerHTML = `Har du brug for hjælp? <span id="funny-smiley">😊</span>` ;
+        messageBox.innerHTML = `Har du brug for hjælp <span id="funny-smiley">😊</span>` ;
       } else {
         messageBox.innerHTML = `Velkommen tilbage! Har du brug for hjælp? <span id="funny-smiley">😄</span>`;
       }
