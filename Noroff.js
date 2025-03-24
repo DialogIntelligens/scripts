578250b67595d0a807623fd2c8f27b75fd3b22ba
Chatbot Dashboard <dashboard@yourdomain.com>
2025-03-24 08:41:41 +0100

    Update chatbot variables from Chatbot.js

--- Noroff.js
+++ Noroff.js
@@ -292,12 +292,12 @@ document.addEventListener('DOMContentLoaded', function() {
       imageAPI: '',
       
       privacyLink: "https://raw.githubusercontent.com/DialogIntelligens/image-hosting/master/Privatlivspolitikk_noroff.pdf",
-      <strong>headerLogoG: "</strong>https://raw.githubusercontent.com/DialogIntelligens/image-hosting/master/chatbot_logo/logo-1741777918040.png",
-      <strong>themeColor: "</strong>#f43000",
-      <strong>headerTitleG: "</strong>Noroff",
-      <strong>headerSubtitleG: "</strong>Du kommuniserer med en kunstig intelligens. Ved å bruke denne chatboten godtar du at det kan oppstå feil, samt at samtalen kan lagres og behandles. Les mer i vår personvernerklæring.",
-      <strong>titleG: "</strong>Noroffs Virtuelle Assistent",
-      <strong>firstMessage: "</strong>Hei😊\nHva kan jeg hjelpe deg med?",
+      <strong><strong>headerLogoG: "</strong>https://raw.githubusercontent.com/DialogIntelligens/image-hosting/master/chatbot_logo/logo-1741777918040.png",
+      <strong><strong>themeColor: "</strong>#f43000",
+      <strong><strong>headerTitleG: "</strong>Noroff",
+      <strong><strong>headerSubtitleG: "</strong>Du kommuniserer med en kunstig intelligens. Ved å bruke denne chatboten godtar du at det kan oppstå feil, samt at samtalen kan lagres og behandles. Les mer i vår personvernerklæring.",
+      <strong><strong>titleG: "</strong>Noroffs Virtuelle Assistent",
+      <strong><strong>firstMessage: "</strong>Hei😊Hva kan jeg hjelpe deg med?",
       isTabletView: window.innerWidth < 1000 && window.innerWidth > 800,
       isPhoneView: window.innerWidth < 800
     };
