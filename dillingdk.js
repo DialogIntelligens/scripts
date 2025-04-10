34424c3c97dcf530d3040d10b6f8fc91849f05ce
Chatbot Dashboard <dashboard@yourdomain.com>
2025-04-10 13:39:35 +0200

    Update chatbot variables from Chatbot.js

--- dillingdk.js
+++ dillingdk.js
@@ -321,9 +321,9 @@ function initChatbot() {
       headerLogoG: "https://raw.githubusercontent.com/DialogIntelligens/image-hosting/master/chatbot_logo/logo-1741613117737.png",
       messageIcon: "https://image-hosting-pi.vercel.app/messageicon.png",
       themeColor: "#000000",
-      headerTitleG: "Jeg er DILLINGs AI-chatbot.",
+      headerTitleG: "Jeg er DILLINGs AI-chatbot",
       headerSubtitleG: "Du skriver med en AI-chatbot. Samtidig accepterer du, at samtalen kan gemmes og behandles for at forbedre din oplevelse. Læs mere i vores privatlivspolitik. Bemærk: chatbotten kan i nogle tilfælde give fejlagtige oplysninger.",
-      titleG: "DILLINGs Chat ",
+      titleG: "DILLINGs chat ",
       firstMessage: "Hej med dig 🙂 Jeg er ny hos DILLING og er stadig under oplæring. Men jeg vil gøre mit bedste, hvis du har spørgsmål omkring DILLING eller vores produkter. Hvordan kan jeg hjælpe dig?  ",
       isTabletView: (window.innerWidth < 1000 && window.innerWidth > 800),
       isPhoneView: (window.innerWidth < 800)
