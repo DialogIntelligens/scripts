e6d3cc01da4a1a87e4334a36dee8b6cfa93481ad
DialogIntelligens <145685451+DialogIntelligens@users.noreply.github.com>
2025-04-16 08:48:18 +0200

    Update dillingdk.js

--- dillingdk.js
+++ dillingdk.js
@@ -337,7 +337,8 @@ function initChatbot() {
       freshdeskConfirmationText: "Tak for din henvendelse, vi vender tilbage hurtigst muligt.",
 
       inputPlaceholder: "Skriv dit spørgsmål her...",
-      ratingMessage: "Fik du besvaret dit spørgmål?"https://image-hosting-pi.vercel.app/Privatlivspolitik_dilling.pdf",
+      ratingMessage: "Fik du besvaret dit spørgmål?",
+      privacyLink: "https://image-hosting-pi.vercel.app/Privatlivspolitik_dilling.pdf",
       titleLogoG: "http://dialogintelligens.dk/wp-content/uploads/2025/01/Dilling_whitemessagelogo-1.png",
       headerLogoG: "https://raw.githubusercontent.com/DialogIntelligens/image-hosting/master/chatbot_logo/logo-1741613117737.png",
       messageIcon: "https://image-hosting-pi.vercel.app/messageicon.png",
