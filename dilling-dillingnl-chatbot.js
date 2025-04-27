3878e7203036dd519f8cf477defb8e91899e483d
DialogIntelligens <145685451+DialogIntelligens@users.noreply.github.com>
2025-04-27 18:09:29 +0200

    Update dilling-dillingnl-chatbot.js

--- dilling-dillingnl-chatbot.js
+++ dilling-dillingnl-chatbot.js
@@ -443,13 +443,10 @@ function initChatbot() {
         
     var popup = document.getElementById("chatbase-message-bubbles");
     var messageBox = document.getElementById("popup-message-box");
-    var userHasVisited = getCookie("userHasVisited");
-    if (!userHasVisited) {
-      setCookie("userHasVisited", "true", 1, ".yourdomain.com");
-      messageBox.innerHTML = `Heb je hulp nodig? <span id="funny-smiley">😊</span>` ;
-    } else {
-      messageBox.innerHTML = `Heb je hulp nodig? <span id="funny-smiley">😄</span>`;
-    }
+
+    const popupText = "Do you need help?";
+    messageBox.innerHTML = `${popupText} <span id="funny-smiley">😊</span>`;
+    
     // Determine popup width based on character count (excluding any HTML tags)
     var charCount = messageBox.textContent.trim().length;
     var popupElem = document.getElementById("chatbase-message-bubbles");
