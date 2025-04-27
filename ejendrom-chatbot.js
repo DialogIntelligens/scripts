028614d0262a3485a3c1f17cd103a504ab53b7dc
DialogIntelligens <145685451+DialogIntelligens@users.noreply.github.com>
2025-04-27 18:24:30 +0200

    Update ejendrom-chatbot.js

--- ejendrom-chatbot.js
+++ ejendrom-chatbot.js
@@ -489,13 +489,10 @@ document.addEventListener('DOMContentLoaded', function() {
         
       var popup = document.getElementById("chatbase-message-bubbles");
       var messageBox = document.getElementById("popup-message-box");
-      var userHasVisited = getCookie("userHasVisited");
-      if (!userHasVisited) {
-        setCookie("userHasVisited", "true", 1, ".yourdomain.com");
-        messageBox.innerHTML = `Har du brug for hjælp med boliger, vurdering, eller til at finde den rette bolig for dig? <span id="funny-smiley">😊</span>` ;
-      } else {
-        messageBox.innerHTML = `Velkommen tilbage! Har du brug for hjælp? <span id="funny-smiley">😄</span>`;
-      }
+
+      const popupText = "Har du brug for hjælp med boliger, vurdering, eller til at finde den rette bolig for dig? ";
+      messageBox.innerHTML = `${popupText} <span id="funny-smiley">😊</span>`;
+      
       // Determine popup width based on character count (excluding any HTML tags)
       var charCount = messageBox.textContent.trim().length;
       var popupElem = document.getElementById("chatbase-message-bubbles");
