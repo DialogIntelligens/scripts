733f801bffbc491e7e95874ae9e0b729c577791f
DialogIntelligens <145685451+DialogIntelligens@users.noreply.github.com>
2025-04-27 18:23:34 +0200

    Update crux-chatbot.js

--- crux-chatbot.js
+++ crux-chatbot.js
@@ -411,13 +411,10 @@ document.addEventListener('DOMContentLoaded', function() {
         
       var popup = document.getElementById("chatbase-message-bubbles");
       var messageBox = document.getElementById("popup-message-box");
-      var userHasVisited = getCookie("userHasVisited");
-      if (!userHasVisited) {
-        setCookie("userHasVisited", "true", 1, ".yourdomain.com");
-        messageBox.innerHTML = `Har du bruk for hjelp? <span id="funny-smiley">😊</span>` ;
-      } else {
-        messageBox.innerHTML = `Velkommen tilbage! Har du brug for hjælp? <span id="funny-smiley">😄</span>`;
-      }
+
+      const popupText = "Do you need help?";
+      messageBox.innerHTML = `${popupText} <span id="funny-smiley">😊</span>`;
+      
       // Determine popup width based on character count (excluding any HTML tags)
       var charCount = messageBox.textContent.trim().length;
       var popupElem = document.getElementById("chatbase-message-bubbles");
