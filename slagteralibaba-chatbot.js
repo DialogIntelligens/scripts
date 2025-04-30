958ba4776f30dc93f449945794ea64f410dc677a
Chatbot Dashboard <dashboard@yourdomain.com>
2025-04-30 16:53:34 +0200

    Update chatbot variables from Users dashboard

--- slagteralibaba-chatbot.js
+++ slagteralibaba-chatbot.js
@@ -219,7 +219,7 @@ document.addEventListener('DOMContentLoaded', function() {
     }
   
     :root {
-      --icon-color: #934237;
+      --icon-color: #121212;
     }
   
     /* The main message content area */
