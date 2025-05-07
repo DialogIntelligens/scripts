1b5464eb3dd135737f43be7322d23387ad9f6512
DialogIntelligens <145685451+DialogIntelligens@users.noreply.github.com>
2025-05-07 14:21:45 +0200

    Update skolenfri-skolenfri-chatbot.js

--- skolenfri-skolenfri-chatbot.js
+++ skolenfri-skolenfri-chatbot.js
@@ -393,7 +393,8 @@ document.addEventListener('DOMContentLoaded', function() {
       freshdeskConfirmationText: "Tak for din henvendelse, vi vender tilbage hurtigst muligt.",
 
       inputPlaceholder: "Skriv dit spørgsmål her...",
-      ratingMessage: "Fik du besvaret dit spørgsmål?"https://raw.githubusercontent.com/DialogIntelligens/image-hosting/master/chatbot_logo/logo-1739887511831.png",
+      ratingMessage: "Fik du besvaret dit spørgsmål?",
+      headerLogoG: "https://raw.githubusercontent.com/DialogIntelligens/image-hosting/master/chatbot_logo/logo-1739887511831.png",
       themeColor: "#69c8e4",
       headerTitleG: "Skolen Fri",
       headerSubtitleG: "Du skriver med en kunstig intelligens. Ved at bruge denne chatbot accepterer du at der kan opstå fejl, og at samtalen kan gemmes og behandles. Læs mere i vores privatlivspolitik.",
