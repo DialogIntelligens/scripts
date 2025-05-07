0ad7af28e5cad102127b3dd21358379a01f7f4b6
Chatbot Dashboard <dashboard@yourdomain.com>
2025-05-07 14:26:09 +0200

    Update chatbot variables from Chatbot.js

--- skolenfri-skolenfri-chatbot.js
+++ skolenfri-skolenfri-chatbot.js
@@ -394,7 +394,7 @@ document.addEventListener('DOMContentLoaded', function() {
 
       inputPlaceholder: "Skriv dit spørgsmål her...",
       ratingMessage: "Fik du besvaret dit spørgsmål?",
-      headerLogoG: "https://raw.githubusercontent.com/DialogIntelligens/image-hosting/master/chatbot_logo/logo-1739887511831.png",
+      headerLogoG: "https://raw.githubusercontent.com/DialogIntelligens/image-hosting/master/chatbot_logo/logo-1746620762879.png",
       themeColor: "#69c8e4",
       headerTitleG: "Skolen Fri",
       headerSubtitleG: "Du skriver med en kunstig intelligens. Ved at bruge denne chatbot accepterer du at der kan opstå fejl, og at samtalen kan gemmes og behandles. Læs mere i vores privatlivspolitik.",
