c04a9a7769379909400a2bb5a999f8a43fadb9a2
Chatbot Dashboard <dashboard@yourdomain.com>
2025-06-03 17:28:53 +0200

    Update chatbot variables from Chatbot.js

--- skoringen-skoringen-chatbot.js
+++ skoringen-skoringen-chatbot.js
@@ -493,7 +493,7 @@ document.addEventListener('DOMContentLoaded', function() {
       headerLogoG: "https://raw.githubusercontent.com/DialogIntelligens/image-hosting/master/chatbot_logo/logo-1748964522916.png",
       messageIcon: "",
       themeColor: "#de5352",
-      headerTitleG: " ",
+      headerTitleG: "Skoringen",
       headerSubtitleG: "Du skriver med en kunstig intelligens. Ved at bruge denne chatbot accepterer du at der kan opstå fejl, og at samtalen kan gemmes og behandles. Læs mere i vores privatlivspolitik.",
       subtitleLinkText: "",
       subtitleLinkUrl: "",
