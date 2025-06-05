f10e042fc339f3ea1a887ee085c0c574eb5fcebb
Chatbot Dashboard <dashboard@yourdomain.com>
2025-06-05 10:17:21 +0200

    Update chatbot variables from Chatbot.js

--- skoringen-skoringen-chatbot.js
+++ skoringen-skoringen-chatbot.js
@@ -490,7 +490,7 @@ document.addEventListener('DOMContentLoaded', function() {
       productButtonText: "SE PRODUKT",
       productImageHeightMultiplier: 1,
         
-      headerLogoG: "https://raw.githubusercontent.com/DialogIntelligens/image-hosting/master/chatbot_logo/logo-1748964810682.png",
+      headerLogoG: "https://raw.githubusercontent.com/DialogIntelligens/image-hosting/master/chatbot_logo/logo-1749111433601.png",
       messageIcon: "",
       themeColor: "#de5352",
       headerTitleG: " ",
