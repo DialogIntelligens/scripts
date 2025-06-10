0c62b54c49944ba2525038716c67bc7dc2f5a093
Chatbot Dashboard <dashboard@yourdomain.com>
2025-06-10 10:14:27 +0200

    Update chatbot variables from Users dashboard

--- koreskolenpaatoppen-koreskolenpaatoppen-chatbot.js
+++ koreskolenpaatoppen-koreskolenpaatoppen-chatbot.js
@@ -462,7 +462,7 @@ document.addEventListener('DOMContentLoaded', function() {
       valutaOverride: "",
       customVar1: "",
       
-      privacyLink: "https://raw.githubusercontent.com/DialogIntelligens/image-hosting/master/Privatlivspolitik_Nih.pdf",
+      privacyLink: "https://raw.githubusercontent.com/DialogIntelligens/image-hosting/master/Privacy_policy_KoreskolenpaaToppen.pdf",
 
       // Set FreshdeskForm text
       freshdeskEmailLabel: "Din email:",
