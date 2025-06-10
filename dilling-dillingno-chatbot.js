6cfcb804bbc9b8826991d70352d9d491d277a345
DialogIntelligens <145685451+DialogIntelligens@users.noreply.github.com>
2025-06-10 09:50:17 +0200

    Update dilling-dillingno-chatbot.js

--- dilling-dillingno-chatbot.js
+++ dilling-dillingno-chatbot.js
@@ -490,7 +490,7 @@ function initChatbot() {
     var popup = document.getElementById("chatbase-message-bubbles");
     var messageBox = document.getElementById("popup-message-box");
 
-    const popupText = "Trenger du hjelp?\\\\\\\\n";
+    const popupText = "Trenger du hjelp?";
     messageBox.innerHTML = `${popupText} <span id="funny-smiley">😊</span>`;
     
     // Determine popup width based on character count (excluding any HTML tags)
