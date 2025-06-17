6b20ea767f91236c024544f7a3cae2be2a9e0728
Chatbot Dashboard <dashboard@yourdomain.com>
2025-06-17 10:39:37 +0200

    Update chatbot variables from Chatbot.js

--- dilling-dillingfi-chatbot.js
+++ dilling-dillingfi-chatbot.js
@@ -548,7 +548,7 @@ function initChatbot() {
     var popup = document.getElementById("chatbase-message-bubbles");
     var messageBox = document.getElementById("popup-message-box");
     
-    const popupText = "Tarvitsetko apua?";
+    const popupText = "Avez-vous besoin d'aide? ";
     messageBox.innerHTML = `${popupText} <span id="funny-smiley">😊</span>`;
     
     // Determine popup width based on character count (excluding any HTML tags)
