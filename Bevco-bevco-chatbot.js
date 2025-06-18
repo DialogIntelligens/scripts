6020945681e60ee412a30806624a4f72ccbcb72b
Chatbot Dashboard <dashboard@yourdomain.com>
2025-06-18 11:59:07 +0200

    Update chatbot variables from Chatbot.js

--- Bevco-bevco-chatbot.js
+++ Bevco-bevco-chatbot.js
@@ -650,7 +650,7 @@ document.addEventListener('DOMContentLoaded', function() {
       var popup = document.getElementById("chatbase-message-bubbles");
       var messageBox = document.getElementById("popup-message-box");
       
-      const popupText = "Jeg kan anbefale drikkevarer, finde ordre og svare på spørgsmål🍾";
+      const popupText = "Jeg kan anbefale drikkevarer, finde ordrer og svare på spørgsmål🍾";
       messageBox.innerHTML = `${popupText}<span id="funny-smiley">😊</span>`;    
       
       // Determine popup width based on character count (excluding any HTML tags)
