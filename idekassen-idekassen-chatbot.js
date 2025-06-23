2b3dc1387c3dbfec9e09ad1073a6afea787bf10d
DialogIntelligens <145685451+DialogIntelligens@users.noreply.github.com>
2025-06-23 09:21:24 +0200

    Update idekassen-idekassen-chatbot.js

--- idekassen-idekassen-chatbot.js
+++ idekassen-idekassen-chatbot.js
@@ -254,7 +254,7 @@ document.addEventListener('DOMContentLoaded', function() {
       position: fixed;
       z-index: 20;
       right: 10px;
-      bottom: 30px;
+      bottom: 60px;
     }
     #chat-button svg {
       width: 60px;
