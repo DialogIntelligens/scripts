c5be9ecd17a910f0eb4855d5bdc14463145f4b8c
DialogIntelligens <145685451+DialogIntelligens@users.noreply.github.com>
2025-06-25 21:45:30 +0200

    Update pursico-chatbot.js

--- pursico-chatbot.js
+++ pursico-chatbot.js
@@ -140,7 +140,7 @@ function purchaseKey(userId) {
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({
         user_id:   chatbotUserId,
-        chatbot_id:'skadedyrshop',
+        chatbot_id:'pursico',
         amount:    totalPrice
       })
     })
