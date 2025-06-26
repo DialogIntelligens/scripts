4b78bc43f107496989fede00f7c6602a5b054bb6
Chatbot Dashboard <dashboard@yourdomain.com>
2025-06-26 08:40:39 +0200

    Update chatbot variables from Users dashboard

--- kirurgiklinik-kirurgiklinik-chatbot.js
+++ kirurgiklinik-kirurgiklinik-chatbot.js
@@ -478,7 +478,7 @@ document.addEventListener('DOMContentLoaded', function() {
       valutaOverride: "",
       customVar1: "",
       
-      privacyLink: "https://raw.githubusercontent.com/DialogIntelligens/image-hosting/master/Privacy_Policy_Kirurgiklinik.pdf",
+      privacyLink: " https://www.kirurgiklinik.dk/patienter/persondatapolitik/",
 
       // Set FreshdeskForm text
       freshdeskEmailLabel: "Din email:",
