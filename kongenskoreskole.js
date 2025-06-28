54ee8b54e1dca458275975096cd69c1f76540604
Chatbot Dashboard <dashboard@yourdomain.com>
2025-06-28 12:19:32 +0200

    Update chatbot variables from Users dashboard

--- kongenskoreskole.js
+++ kongenskoreskole.js
@@ -355,7 +355,7 @@ document.addEventListener('DOMContentLoaded', function() {
         replaceExclamationWithPeriod: false,
   
         pineconeApiKey: "pcsk_5DhXSe_3TDYCHkg8bdNL2PBgprKCbm1XeewQMnr84fsm18eMH7dgDxxrHKQhxntJduLELJ",
-        knowledgebaseIndexApiEndpoint: "koreskole",
+        knowledgebaseIndexApiEndpoint: "kongen-alt",
         flow2KnowledgebaseIndex: "",
         flow3KnowledgebaseIndex: "",
         flow4KnowledgebaseIndex: "",
