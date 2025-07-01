7c614b8d6c82f9adf8bc7132bc38f6042f26719f
Chatbot Dashboard <dashboard@yourdomain.com>
2025-07-01 11:44:34 +0200

    Update chatbot variables from Users dashboard

--- yaay-yaay-chatbot.js
+++ yaay-yaay-chatbot.js
@@ -487,7 +487,7 @@ document.addEventListener('DOMContentLoaded', function() {
         chatbotID: "yaay",
         pagePath: window.location.href,
         statestikAPI: "https://den-utrolige-snebold.onrender.com/api/v1/prediction/895ebf87-a105-412e-b376-2ccaa9b0cdf6",
-        fordelingsflowAPI: "https://den-utrolige-snebold.onrender.com/api/v1/prediction/4f520e9d-77fb-4f5e-b851-10cf5a73b05c",
+        fordelingsflowAPI: "",
         flow2Key: "",
         flow2API: "",
         flow3Key: "product",
