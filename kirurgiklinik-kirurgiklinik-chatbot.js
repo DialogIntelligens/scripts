6115dfd39ce5135aadf4cbfa6f9b16f07e3e151e
Chatbot Dashboard <dashboard@yourdomain.com>
2025-07-04 12:31:33 +0200

    Update chatbot variables from Chatbot.js

--- kirurgiklinik-kirurgiklinik-chatbot.js
+++ kirurgiklinik-kirurgiklinik-chatbot.js
@@ -507,7 +507,7 @@ document.addEventListener('DOMContentLoaded', function() {
       productImageHeightMultiplier: 1,
         
       headerLogoG: "https://raw.githubusercontent.com/DialogIntelligens/image-hosting/master/chatbot_logo/logo-1749546748940.png",
-      messageIcon: "",
+      messageIcon: "https://raw.githubusercontent.com/DialogIntelligens/image-hosting/master/chatbot_message_icon/logo-1751625091393.png",
       themeColor: "#426587",
       headerTitleG: "Hejsa",
       headerSubtitleG: "Du skriver med Liv, Kirurgiklinikkens virtuelle assistent. Ved brug af denne chatbot accepterer du, at der kan opstå fejl, og at samtalen kan gemmes og behandles. Bemærk venligst, at vi fraråder at dele personfølsomme oplysninger som f.eks. helbredsdata eller CPR-nummer. Læs mere i vores privatlivspolitik.",
