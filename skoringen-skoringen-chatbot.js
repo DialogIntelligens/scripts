de63657a7032cbafe273d95ce9846cd603a4a324
Chatbot Dashboard <dashboard@yourdomain.com>
2025-07-07 09:28:34 +0200

    Update chatbot variables from Chatbot.js

--- skoringen-skoringen-chatbot.js
+++ skoringen-skoringen-chatbot.js
@@ -801,7 +801,7 @@ function trackChatbotOpen() {
       var popup = document.getElementById("chatbase-message-bubbles");
       var messageBox = document.getElementById("popup-message-box");
       
-      const popupText = "Jeg kan anbefale sko  og besvare sprøgsmål👟 ";
+      const popupText = "Jeg kan anbefale sko  og besvare sprøgsmål👟";
       messageBox.innerHTML = `${popupText} <span id="funny-smiley">😊</span>`;    
       
       // Determine popup width based on character count (excluding any HTML tags)
