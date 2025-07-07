3ae47f89d6bdd344fe7e22f5bea66eb1385f6ee9
Chatbot Dashboard <dashboard@yourdomain.com>
2025-07-07 09:40:12 +0200

    Update chatbot variables from Chatbot.js

--- skoringen-skoringen-chatbot.js
+++ skoringen-skoringen-chatbot.js
@@ -573,12 +573,12 @@ setInterval(checkForPurchase, 15000); // Check every 15 seconds
       messageIcon: "https://raw.githubusercontent.com/DialogIntelligens/image-hosting/master/slogo.png",
       themeColor: "#000000",
       headerTitleG: "",
-      headerSubtitleG: "Jeg er din digitale assistent og ved en hel del om sko, størrelser, brands og hvordan du handler hos Skoringen - både online og i butik.\nSpørg endelig løs, så prøver jeg at hjælpe dig så godt jeg kan. Men husk, jeg er kun en robot - så hvis noget er uklart eller ikke giver mening, kan du altid tage fat i vores kundeservice 💬",
+      headerSubtitleG: "Jeg er din digitale assistent og ved en hel del om sko, størrelser, brands og hvordan du handler hos Skoringen - både online og i butik.\\nSpørg endelig løs, så prøver jeg at hjælpe dig så godt jeg kan. Men husk, jeg er kun en robot - så hvis noget er uklart eller ikke giver mening, kan du altid tage fat i vores kundeservice 💬",
       subtitleLinkText: "",
       subtitleLinkUrl: "",
         
       titleG: "Skobot - Din digitale AI assistent",
-      firstMessage: "Hej! Jeg hedder Skobot 😊\nJeg er din digitale AI assistent og står klar til at hjælpe dig med spørgsmål om produkter, åbningstider og vores services.\n\nTøv ikke med at skrive, hvis du har brug for hjælp!\nNår du skriver til mig, accepterer du samtidig, at vores samtale bliver behandlet og gemt. 🤖\n\n",
+      firstMessage: "Hej! Jeg hedder Skobot 😊\nJeg er din digitale AI assistent og står klar til at hjælpe dig med spørgsmål om produkter, åbningstider og vores services.\n\nTøv ikke med at skrive, hvis du har brug for hjælp!\\nNår du skriver til mig, accepterer du samtidig, at vores samtale bliver behandlet og gemt. 🤖\\n\\n",
       parentWebsiteUserId: websiteUserId,
       isTabletView: window.innerWidth < 1000 && window.innerWidth > 800,
       isPhoneView: window.innerWidth < 800
