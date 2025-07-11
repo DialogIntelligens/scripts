6ea9b97cb5cfa32281beb786c862e8a4e730eebb
Chatbot Dashboard <dashboard@yourdomain.com>
2025-07-11 15:02:11 +0200

    Update chatbot variables from Users dashboard

--- vesterlyng-vesterlyng-chatbot.js
+++ vesterlyng-vesterlyng-chatbot.js
@@ -524,7 +524,7 @@ setInterval(checkForPurchase, 15000); // Check every 15 seconds
       pineconeApiKey: "",
       knowledgebaseIndexApiEndpoint: "vesterlyng-alt",
       flow2KnowledgebaseIndex: "",
-      flow3KnowledgebaseIndex: "",
+      flow3KnowledgebaseIndex: "vesterlyng-pro",
       flow4KnowledgebaseIndex: "",
       apiFlowKnowledgebaseIndex: "",
       websiteOverride: "",
