7386e5a09b9b55f932062cf3d636ed9bfd00dc66
Chatbot Dashboard <dashboard@yourdomain.com>
2025-07-11 14:47:05 +0200

    Update chatbot variables from Users dashboard

--- vesterlyng-vesterlyng-chatbot.js
+++ vesterlyng-vesterlyng-chatbot.js
@@ -369,7 +369,7 @@ setInterval(checkForPurchase, 15000); // Check every 15 seconds
 
   
     :root {
-      --icon-color: #000000;
+      --icon-color: #224e9a;
       --badge-color: #CC2B20;
     }
     
