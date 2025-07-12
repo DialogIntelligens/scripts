f6dff87610f33e4e520347a8981c655e2e11440b
DialogIntelligens <145685451+DialogIntelligens@users.noreply.github.com>
2025-07-12 07:51:18 +0200

    Update marienlyst-marienlyst-chatbot.js

--- marienlyst-marienlyst-chatbot.js
+++ marienlyst-marienlyst-chatbot.js
@@ -492,6 +492,8 @@ document.addEventListener('DOMContentLoaded', function() {
       headerLogoG: "https://raw.githubusercontent.com/DialogIntelligens/image-hosting/master/chatbot_logo/logo-1748970662647.png",
       messageIcon: "",
       themeColor: "#49868f",
+            aiMessageColor: "#e9ecef",
+          aiMessageTextColor: "#000000",
       headerTitleG: " ",
       headerSubtitleG: "Du skriver med en kunstig intelligens. Ved at bruge denne chatbot accepterer du at der kan opstå fejl, og at samtalen kan gemmes og behandles. Læs mere i vores privatlivspolitik.",
       subtitleLinkText: "",
