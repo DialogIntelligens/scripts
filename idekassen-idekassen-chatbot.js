7640ca562612b90d443f1bf96b1500402d098b25
DialogIntelligens <145685451+DialogIntelligens@users.noreply.github.com>
2025-07-12 07:49:35 +0200

    Update idekassen-idekassen-chatbot.js

--- idekassen-idekassen-chatbot.js
+++ idekassen-idekassen-chatbot.js
@@ -528,6 +528,8 @@ document.addEventListener('DOMContentLoaded', function() {
         
       headerLogoG: "https://raw.githubusercontent.com/DialogIntelligens/image-hosting/master/chatbot_logo/logo-1747731434213.png",
       themeColor: "#d65900",
+            aiMessageColor: "#e9ecef",
+          aiMessageTextColor: "#000000",
       headerTitleG: "Idekassen",
       headerSubtitleG: "Du skriver med en kunstig intelligens. Ved at bruge denne chatbot accepterer du at der kan opstå fejl, og at samtalen kan gemmes og behandles. Læs mere i vores privatlivspolitik.",
       subtitleLinkText: "",
