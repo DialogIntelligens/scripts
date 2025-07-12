41fc3f983aff43fbc0075da44a57029d48127364
DialogIntelligens <145685451+DialogIntelligens@users.noreply.github.com>
2025-07-12 07:52:59 +0200

    Update slagteralibaba-chatbot.js

--- slagteralibaba-chatbot.js
+++ slagteralibaba-chatbot.js
@@ -362,6 +362,8 @@ document.addEventListener('DOMContentLoaded', function() {
       privacyLink: "https://raw.githubusercontent.com/DialogIntelligens/image-hosting/master/slagteralibaba.pdf",
       headerLogoG: "https://raw.githubusercontent.com/DialogIntelligens/image-hosting/master/chatbot_logo/logo-1744704790777.png",
       themeColor: "#121212",
+            aiMessageColor: "#e9ecef",
+          aiMessageTextColor: "#000000",
       headerTitleG: " ",
       headerSubtitleG: "Du skriver med en kunstig intelligens. Ved at bruge denne chatbot accepterer du at der kan opstå fejl, og at samtalen kan gemmes og behandles. Læs mere i vores privatlivspolitik.",
       subtitleLinkText: "",
