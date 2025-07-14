42ee3e9791b8b82cba720dfed6bbda3a8c18a9d4
Chatbot Dashboard <dashboard@yourdomain.com>
2025-07-14 21:27:36 +0200

    Update chatbot variables from Users dashboard

--- naturmand.js
+++ naturmand.js
@@ -110,7 +110,7 @@ document.addEventListener('DOMContentLoaded', function() {
       pineconeApiKey: "pcsk_t4xAk_H8wDYbReN7o4WUTjziM8q5L9hX1dDkactXPfN7XxrYpRj7VNS4jXRgo32GeyiPM",
       knowledgebaseIndexApiEndpoint: "naturmand",
       flow2KnowledgebaseIndex: "",
-      flow3KnowledgebaseIndex: "",
+      flow3KnowledgebaseIndex: "naturmand-pro",
       flow4KnowledgebaseIndex: "",
       apiFlowKnowledgebaseIndex: "",
       
