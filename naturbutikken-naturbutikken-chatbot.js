97613fdcca7b45caf56eb1e65da6d0636f9532e7
Chatbot Dashboard <dashboard@yourdomain.com>
2025-07-14 14:13:03 +0200

    Update chatbot variables from Users dashboard

--- naturbutikken-naturbutikken-chatbot.js
+++ naturbutikken-naturbutikken-chatbot.js
@@ -369,7 +369,7 @@ setInterval(checkForPurchase, 15000); // Check every 15 seconds
 
   
     :root {
-      --icon-color: #000000;
+      --icon-color: #3f6aa4;
       --badge-color: #CC2B20;
     }
     
@@ -562,17 +562,17 @@ setInterval(checkForPurchase, 15000); // Check every 15 seconds
       productButtonText: "SE PRODUKT",
       productImageHeightMultiplier: 1,
         
-      headerLogoG: "https://raw.githubusercontent.com/DialogIntelligens/image-hosting/master/chatbot_logo/logo-1739887511831.png",
+      headerLogoG: "https://raw.githubusercontent.com/DialogIntelligens/image-hosting/master/chatbot_logo/logo-1752495029112.png",
       messageIcon: "",
-      themeColor: "#374c51",
+      themeColor: "#3f6aa4",
       aiMessageColor: "#e9ecef",
       aiMessageTextColor: "#000000",
-      headerTitleG: "Nordjyllands Idrætshøjskole",
+      headerTitleG: "",
       headerSubtitleG: "Du skriver med en kunstig intelligens. Ved at bruge denne chatbot accepterer du at der kan opstå fejl, og at samtalen kan gemmes og behandles. Læs mere i vores privatlivspolitik.",
       subtitleLinkText: "",
       subtitleLinkUrl: "",
         
-      titleG: "NIH's Virtuelle Assistent",
+      titleG: "Natur Butikken AI",
       firstMessage: "Hej 😊 Spørg mig om alt – lige fra produkter til generelle spørgsmål, eller få personlige anbefalinger 🤖",
       purchaseTrackingEnabled: true,
       isTabletView: false,
