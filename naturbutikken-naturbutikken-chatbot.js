e1b7f0fa0c6ff6e0331daa7b26b3375381e053d4
DialogIntelligens <145685451+DialogIntelligens@users.noreply.github.com>
2025-07-17 12:08:28 +0200

    Update naturbutikken-naturbutikken-chatbot.js

--- naturbutikken-naturbutikken-chatbot.js
+++ naturbutikken-naturbutikken-chatbot.js
@@ -795,7 +795,7 @@ function trackChatbotOpen() {
       var popup = document.getElementById("chatbase-message-bubbles");
       var messageBox = document.getElementById("popup-message-box");
       
-      const popupText = "Har du brug for hjælp? ";
+      const popupText = "Jeg kan anbefale produkter og besvare spørgsmål 🐦";
       messageBox.innerHTML = `${popupText} <span id="funny-smiley">😊</span>`;    
       
       // Determine popup width based on character count (excluding any HTML tags)
