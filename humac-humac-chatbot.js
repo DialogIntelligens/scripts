8ef4a5164e0922b7d934a0d1a4d74cdb663d8a3a
Chatbot Dashboard <dashboard@yourdomain.com>
2025-08-01 18:53:01 +0200

    Update chatbot variables from Users dashboard

--- humac-humac-chatbot.js
+++ humac-humac-chatbot.js
@@ -521,7 +521,7 @@ setInterval(checkForPurchase, 15000); // Check every 15 seconds
       ratingTimerDuration: 15000,
       replaceExclamationWithPeriod: false,
 
-      pineconeApiKey: "",
+      pineconeApiKey: "pcsk_2Pv6s6_B2XcjJmDsi7ZCYXqwph4wo18vsGQMTR7ThkWhQ36M5JuMWoKh4kAGG57KHnfXge",
       knowledgebaseIndexApiEndpoint: "hjemmeside3",
       flow2KnowledgebaseIndex: "",
       flow3KnowledgebaseIndex: "",
