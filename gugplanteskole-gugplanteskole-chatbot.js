cbfb2d750a884aedd131c31c2ba3eec57462f3a8
Chatbot Dashboard <dashboard@yourdomain.com>
2025-08-13 11:37:02 +0200

    Update chatbot variables from Chatbot.js

--- gugplanteskole-gugplanteskole-chatbot.js
+++ gugplanteskole-gugplanteskole-chatbot.js
@@ -573,7 +573,7 @@ setInterval(checkForPurchase, 15000); // Check every 15 seconds
       subtitleLinkUrl: "",
         
       titleG: " Din AI-gartner hos Gug 🌼",
-      firstMessage: "Hej 🌿 Jeg er Gro – din AI-gartner hos Gug Planteskole. Spørg mig om blomster, jord, beskæring eller plantesygdomme – jeg finder svar fra vores hjemmeside. Kan jeg ikke hjælpe, så kontakt mine menneske-kollegaer på mail eller telefon 📞",
+      firstMessage: "Hej 🌿 Jeg er Gro – din AI-gartner hos Gug Planteskole. Spørg mig om alt fra planter, gødning, beskæring og plantesygdomme til meget mere – jeg finder svar fra vores hjemmeside. Kan jeg ikke hjælpe, kan du kontakte mine menneske-kollegaer 📞",
       purchaseTrackingEnabled: true,
       isTabletView: false,
       isPhoneView: window.innerWidth < 1000
