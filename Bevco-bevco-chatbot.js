5582d8471e043f04ae0d27964cf5a67ac01ffe98
DialogIntelligens <145685451+DialogIntelligens@users.noreply.github.com>
2025-08-20 16:57:51 +0200

    Update Bevco-bevco-chatbot.js

--- Bevco-bevco-chatbot.js
+++ Bevco-bevco-chatbot.js
@@ -874,7 +874,7 @@ function adjustIframeSize() {
         iframe.style.width = '95vw';
         iframe.style.height = '90vh';
     } else {
-        iframe.style.width = 'calc(45vh + 6vw)'; // Restoring your old width calculation
+        iframe.style.width = 'calc(50vh + 8vw)'; // Restoring your old width calculation
         iframe.style.height = '90vh';
     }
   
