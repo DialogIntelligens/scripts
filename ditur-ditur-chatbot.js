d88e10f62b8da7e8cd051344ed397bda8b00d9ac
Chatbot Dashboard <dashboard@yourdomain.com>
2025-08-22 11:54:21 +0200

    Update chatbot variables from Users dashboard

--- ditur-ditur-chatbot.js
+++ ditur-ditur-chatbot.js
@@ -562,7 +562,7 @@ setInterval(checkForPurchase, 15000); // Check every 15 seconds
       productButtonText: "SE PRODUKT",
       productImageHeightMultiplier: 1,
         
-      headerLogoG: "https://raw.githubusercontent.com/DialogIntelligens/image-hosting/master/chatbot_logo/logo-1755359530671.png",
+      headerLogoG: "https://raw.githubusercontent.com/DialogIntelligens/image-hosting/master/chatbot_logo/logo-1755856456554.png",
       messageIcon: "https://raw.githubusercontent.com/DialogIntelligens/image-hosting/master/chatbot_message_icon/logo-1755359826156.png",
       themeColor: "#212e48",
       aiMessageColor: "#d8d8d8",
