f244159ba30c563d4c2b98c6c67ba34991bc71fb
Chatbot Dashboard <dashboard@yourdomain.com>
2025-09-01 16:46:09 +0200

    Update chatbot variables from Users dashboard

--- storeandstefterskole-storeandstefterskole-chatbot.js
+++ storeandstefterskole-storeandstefterskole-chatbot.js
@@ -653,7 +653,7 @@ setInterval(checkForPurchase, 15000); // Check every 15 seconds
         
       headerLogoG: "https://raw.githubusercontent.com/DialogIntelligens/image-hosting/master/chatbot_logo/logo-1756407227158.png",
       messageIcon: "https://raw.githubusercontent.com/DialogIntelligens/image-hosting/master/chatbot_message_icon/logo-1755696262145.png",
-      themeColor: "#e5d58f",
+      themeColor: "#444440",
       aiMessageColor: "#f2eee2",
       aiMessageTextColor: "#000000",
       headerTitleG: "AI medhjælper 🤖",
