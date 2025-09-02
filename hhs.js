f230ea797fe6517eef0c4390607320efbae547ef
Chatbot Dashboard <dashboard@yourdomain.com>
2025-09-02 16:49:38 +0200

    Update chatbot variables from Users dashboard

--- hhs.js
+++ hhs.js
@@ -746,9 +746,7 @@ function trackChatbotOpen() {
         
       var popup = document.getElementById("chatbase-message-bubbles");
       var messageBox = document.getElementById("popup-message-box");
-      
-      const popupText = "Jeg kan anbefale produkter og hjælpe med dine skadedyr🐜 ";
-      messageBox.innerHTML = `${popupText} <span id="funny-smiley">😊</span>`;    
+messageBox.innerHTML = `${popupText} <span id="funny-smiley">😊</span>`;    
       
       // Determine popup width based on character count (excluding any HTML tags)
       var charCount = messageBox.textContent.trim().length;
