790a10ec7cbcda74888fb4d0535c540746c394df
DialogIntelligens <145685451+DialogIntelligens@users.noreply.github.com>
2025-09-09 12:04:10 +0200

    Update vinhuset-vinhuset_fi-chatbot.js

--- vinhuset-vinhuset_fi-chatbot.js
+++ vinhuset-vinhuset_fi-chatbot.js
@@ -800,7 +800,8 @@ function trackChatbotOpen() {
         
       var popup = document.getElementById("chatbase-message-bubbles");
       var messageBox = document.getElementById("popup-message-box");
-messageBox.innerHTML = `${popupText}<span id="funny-smiley">😊</span>`;    
+      const popupText = "Jeg kan anbefale produkter og besvare spørgsmål ";
+      messageBox.innerHTML = `${popupText} <span id="funny-smiley">😊</span>`;   
       
       // Determine popup width based on character count (excluding any HTML tags)
       var charCount = messageBox.textContent.trim().length;
