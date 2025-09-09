7dbc253f02be191d146cd35d669a94e54920c071
Chatbot Dashboard <dashboard@yourdomain.com>
2025-09-09 15:19:22 +0200

    Update chatbot variables from Users dashboard

--- vinhuset-vinhuset-chatbot.js
+++ vinhuset-vinhuset-chatbot.js
@@ -800,8 +800,7 @@ function trackChatbotOpen() {
         
       var popup = document.getElementById("chatbase-message-bubbles");
       var messageBox = document.getElementById("popup-message-box");
-      const popupText = "Jeg kan anbefale vine, besvare spørgsmål og se ordrer🍷";
-      messageBox.innerHTML = `${popupText} <span id="funny-smiley">😊</span>`;    
+messageBox.innerHTML = `${popupText} <span id="funny-smiley">😊</span>`;    
       
       // Determine popup width based on character count (excluding any HTML tags)
       var charCount = messageBox.textContent.trim().length;
