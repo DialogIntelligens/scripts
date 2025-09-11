4ee979098f7753c1128bf7dd5ab13118872bcd9b
Chatbot Dashboard <dashboard@yourdomain.com>
2025-09-11 14:10:43 +0200

    Update chatbot variables from Users dashboard

--- ditur-ditur-chatbot.js
+++ ditur-ditur-chatbot.js
@@ -527,7 +527,7 @@ setInterval(checkForPurchase, 15000); // Check every 15 seconds
       flow2KnowledgebaseIndex: "",
       flow3KnowledgebaseIndex: "ditur-pro",
       flow4KnowledgebaseIndex: "",
-      apiFlowKnowledgebaseIndex: "",
+      apiFlowKnowledgebaseIndex: "ditur-alt",
       websiteOverride: "",
       languageOverride: "",
       valutaOverride: "",
@@ -797,7 +797,7 @@ function trackChatbotOpen() {
       var popup = document.getElementById("chatbase-message-bubbles");
       var messageBox = document.getElementById("popup-message-box");
       
-      const popupText = "Jeg kan anbefale produkter og besvare spørgsmål";
+      const popupText = "Jeg kan anbefale produkter og besvare spørgsmålJeg kan anbefale produkter og besvare spørgsmål;;
       messageBox.innerHTML = `${popupText} <span id=;funny-smiley;>😊</span>`;    
       
       // Determine popup width based on character count (excluding any HTML tags)
