dcce81c2ab5d6a1af8114187303f66e3f2c6c6b6
Chatbot Dashboard <dashboard@yourdomain.com>
2025-09-11 13:25:31 +0200

    Update chatbot variables from Users dashboard

--- ditur-ditur-chatbot.js
+++ ditur-ditur-chatbot.js
@@ -796,8 +796,8 @@ function trackChatbotOpen() {
       var popup = document.getElementById("chatbase-message-bubbles");
       var messageBox = document.getElementById("popup-message-box");
       
-      const popupText = "Jeg kan anbefale produkter og besvare spørgsmål Jeg kan anbefale produkter og besvare spørgsmål ;;\n      messageBox.innerHTML = `${popupText} <span id=Jeg kan anbefale produkter og besvare spørgsmål Jeg kan anbefale produkter og besvare spørgsmål ;;
-      messageBox.innerHTML = `${popupText} <span id=;funny-smiley">😊</span>`;    
+      const popupText = "Jeg kan anbefale produkter og besvare spørgsmål Jeg kan anbefale produkter og besvare spørgsmål ;;\n      messageBox.innerHTML = `${popupText} <span id=Jeg kan anbefale produkter og besvare spørgsmål Jeg kan anbefale produkter og besvare spørgsmål ;;\n      messageBox.innerHTML = `${popupText} <span id=Jeg kan anbefale produkter og besvare spørgsmål Jeg kan anbefale produkter og besvare spørgsmål ;;
+      messageBox.innerHTML = `${popupText} <span id=;funny-smiley;>😊</span>`;    
       
       // Determine popup width based on character count (excluding any HTML tags)
       var charCount = messageBox.textContent.trim().length;
