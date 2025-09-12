550d965d88cdf3d002218d1db3a693e34735cefe
DialogIntelligens <145685451+DialogIntelligens@users.noreply.github.com>
2025-09-12 10:18:02 +0200

    Update vinhuset-vinhuset_se-chatbot.js

--- vinhuset-vinhuset_se-chatbot.js
+++ vinhuset-vinhuset_se-chatbot.js
@@ -800,7 +800,7 @@ function trackChatbotOpen() {
         
       var popup = document.getElementById("chatbase-message-bubbles");
       var messageBox = document.getElementById("popup-message-box");
-      const popupText = "Jag kan rekommendera viner, svara på frågor och hitta beställningar 🍷Jag kan rekommendera viner, svara på frågor och hitta beställningar 🍷;
+      const popupText = "Jag kan rekommendera viner, svara på frågor och hitta beställningar 🍷";
       messageBox.innerHTML = `${popupText} <span id="funny-smiley">😊</span>`; 
       
       // Determine popup width based on character count (excluding any HTML tags)
