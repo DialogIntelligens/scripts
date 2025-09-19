77c7e4de99154c884491b8e2bf75b304c590591b
DialogIntelligens <145685451+DialogIntelligens@users.noreply.github.com>
2025-09-19 13:34:20 +0200

    Update aiasound-aiasound-chatbot.js

--- aiasound-aiasound-chatbot.js
+++ aiasound-aiasound-chatbot.js
@@ -443,7 +443,7 @@ document.addEventListener('DOMContentLoaded', function() {
       fordelingsflowAPI: "",
       flow2Key: "",
       flow2API: "",
-      flow3Key: "product",
+      flow3Key: "",
       flow3API: "",
       flow4API: "",
       flow4Key: "",
