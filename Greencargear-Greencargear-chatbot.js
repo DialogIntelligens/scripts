f514409a21b0efd80d156392540a9d00a47f1fc4
Chatbot Dashboard <dashboard@yourdomain.com>
2025-09-23 21:08:30 +0200

    Update chatbot variables from Chatbot.js

--- Greencargear-Greencargear-chatbot.js
+++ Greencargear-Greencargear-chatbot.js
@@ -605,8 +605,8 @@ setInterval(checkForPurchase, 15000); // Check every 15 seconds
       subtitleLinkText: "",
       subtitleLinkUrl: "",
         
-      titleG: "Support-botten Elmer",
-      firstMessage: "Hej, mit navn er Elmer, jeg er GreenCarGears AI chatbot. Jeg kan hjælpe dig med produktspørgsmål eller generelle spørgsmål omkring vores webshop ⚡️",
+      titleG: "Online Support",
+      firstMessage: "Hej, mit navn er ELmer, jeg er GreenCarGears AI chatbot. Jeg kan hjælpe dig med produktspørgsmål eller generelle spørgsmål omkring vores webshop ⚡️",
       purchaseTrackingEnabled: true,
       isTabletView: false,
       isPhoneView: window.innerWidth < 1000,
