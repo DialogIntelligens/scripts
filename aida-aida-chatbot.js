46b4c78d33fcc7f154c8537dffdd9b746050a32a
DialogIntelligens <145685451+DialogIntelligens@users.noreply.github.com>
2025-10-07 14:28:14 +0200

    Update aida-aida-chatbot.js

--- aida-aida-chatbot.js
+++ aida-aida-chatbot.js
@@ -795,7 +795,7 @@ function trackChatbotOpen() {
       if (charCount < 25) {
         popupElem.style.width = "40px";
       } else if (charCount < 60) {
-        popupElem.style.width = "405px";
+        popupElem.style.width = "460px";
       } else {
         popupElem.style.width = "460px";
       }
