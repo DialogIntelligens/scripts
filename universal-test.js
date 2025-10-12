43388c44b9da20a4ed5709c3569d398daa904f29
DialogIntelligens <145685451+DialogIntelligens@users.noreply.github.com>
2025-10-12 23:40:16 +0200

    Update universal-test.js

--- universal-test.js
+++ universal-test.js
@@ -1,1 +1,7 @@
-<script src="https://dialogintelligens.github.io/scripts/universal-chatbot.js?id=boozt"></script>
+// boozt-boozt-chatbot.js
+(function() {
+  const s = document.createElement('script');
+  s.src = "https://dialogintelligens.github.io/scripts/universal-chatbot.js?id=boozt";
+  s.defer = true;
+  document.head.appendChild(s);
+})();
