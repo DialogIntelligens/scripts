3afa1f9a21c6b7fcc9a3b1f8c811eba0cbe2b51d
DialogIntelligens <145685451+DialogIntelligens@users.noreply.github.com>
2025-10-14 09:45:09 +0200

    Update vibholm-vibholm-chatbot.js

--- vibholm-vibholm-chatbot.js
+++ vibholm-vibholm-chatbot.js
@@ -290,7 +290,7 @@ setInterval(checkForPurchase, 15000); // Check every 15 seconds
       position: fixed;
       z-index: 20;
       right: 10px;
-      bottom: 20px;
+      bottom: 25px;
     }
     #chat-button svg {
       width: 65px;
