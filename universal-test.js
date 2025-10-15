c30272cfcc9a05099bc8eeab4f1b80a5e36d053d
DialogIntelligens <145685451+DialogIntelligens@users.noreply.github.com>
2025-10-15 17:02:27 +0200

    Delete universal-test.js

--- universal-test.js
+++
@@ -1,7 +0,0 @@
-// boozt-boozt-chatbot.js
-(function() {
-  const s = document.createElement('script');
-  s.src = "https://dialogintelligens.github.io/scripts/universal-chatbot.js?id=nih";
-  s.defer = true;
-  document.head.appendChild(s);
-})();
