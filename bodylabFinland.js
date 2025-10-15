a40f99e7fea187a6f83dc4fef3b3e7336d6f7976
DialogIntelligens <145685451+DialogIntelligens@users.noreply.github.com>
2025-10-15 12:41:38 +0200

    Update bodylabFinland.js

--- bodylabFinland.js
+++ bodylabFinland.js
@@ -1,6 +1,6 @@
 (function() {
   const s = document.createElement('script');
-  s.src = "https://dialogintelligens.github.io/scripts/universal-chatbot.js?id=bodylabfindland";
+  s.src = "https://dialogintelligens.github.io/scripts/universal-chatbot.js?id=bodylabfinland";
   s.defer = true;
   document.head.appendChild(s);
 })();
