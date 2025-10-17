dc86fb39439807233d0fa5bf13d0cc7bea7a9ada
DialogIntelligens <145685451+DialogIntelligens@users.noreply.github.com>
2025-10-17 09:34:20 +0200

    Update universal-chatbot.js

--- universal-chatbot.js
+++ universal-chatbot.js
@@ -596,7 +596,7 @@
         
         /* Longer message styling */
         #chatbase-message-bubbles.long-message {
-          bottom: calc(${config.buttonBottom || '10px'} + -7.5px);
+          bottom: calc(${config.buttonBottom || '10px'} + -9.5px);
           right: calc(${config.buttonRight || '10px'} + 35px);
           scale: 0.52;
         }
