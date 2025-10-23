f43a27bff36f2b21ff6d21c82a1f0414962d4df4
DialogIntelligens <145685451+DialogIntelligens@users.noreply.github.com>
2025-10-23 12:13:54 +0200

    Update and rename ecooking-ecooking-chatbot.js to ecokoing-ecooking-chatbot.js

---
+++ ecokoing-ecooking-chatbot.js
@@ -0,0 +1,6 @@
+(function() {
+  const s = document.createElement('script');
+  s.src = "https://dialogintelligens.github.io/scripts/universal-chatbot.js?id=ecokoing";
+  s.defer = true;
+  document.head.appendChild(s);
+})();
