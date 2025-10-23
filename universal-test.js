98a9b9f9c2d19432a5f96b5da856db61210a0684
DialogIntelligens <145685451+DialogIntelligens@users.noreply.github.com>
2025-10-23 10:18:26 +0200

    Create universal-test.js

---
+++ universal-test.js
@@ -0,0 +1,6 @@
+(function() {
+  const s = document.createElement('script');
+  s.src = "https://dialogintelligens.github.io/scripts/universal-local.js?id=vinhuset";
+  s.defer = true;
+  document.head.appendChild(s);
+})();
