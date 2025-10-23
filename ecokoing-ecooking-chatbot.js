3ffa7499a330eaae125de0a131cb7ad4a5fb8a05
DialogIntelligens <145685451+DialogIntelligens@users.noreply.github.com>
2025-10-23 12:16:24 +0200

    Update ecokoing-ecooking-chatbot.js

--- ecokoing-ecooking-chatbot.js
+++ ecokoing-ecooking-chatbot.js
@@ -1,6 +1,6 @@
 (function() {
   const s = document.createElement('script');
-  s.src = "https://dialogintelligens.github.io/scripts/universal-chatbot.js?id=ecokoing";
+  s.src = "https://dialogintelligens.github.io/scripts/universal-chatbot.js?id=ecooking";
   s.defer = true;
   document.head.appendChild(s);
 })();
