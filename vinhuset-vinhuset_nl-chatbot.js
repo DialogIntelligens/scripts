3c8c7d98ac36dcfd180dcfd67e30cba5e5358c26
DialogIntelligens <145685451+DialogIntelligens@users.noreply.github.com>
2025-10-30 15:23:13 +0100

    Create vinhuset-vinhuset_nl-chatbot.js

---
+++ vinhuset-vinhuset_nl-chatbot.js
@@ -0,0 +1,6 @@
+(function() {
+  const s = document.createElement('script');
+  s.src = "https://dialogintelligens.github.io/scripts/universal-chatbot.js?id=vinhuset_nl";
+  s.defer = true;
+  document.head.appendChild(s);
+})();
