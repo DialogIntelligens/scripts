9cbd0a5a00190fd0c797acf0fd5da7d846f86edf
DialogIntelligens <145685451+DialogIntelligens@users.noreply.github.com>
2025-11-11 16:04:50 +0100

    Update ditur-ditur-no-chatbot.js

--- ditur-ditur-no-chatbot.js
+++ ditur-ditur-no-chatbot.js
@@ -1,6 +1,6 @@
 (function() {
   const s = document.createElement('script');
-  s.src = "https://dialogintelligens.github.io/scripts/universal-chatbot.js?id=ditur-fi";
+  s.src = "https://dialogintelligens.github.io/scripts/universal-chatbot.js?id=ditur-no";
   s.defer = true;
   document.head.appendChild(s);
 })();
