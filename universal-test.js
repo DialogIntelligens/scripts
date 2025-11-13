e83bc122d0c6f0540887394a8358c2abe3ed3240
TipJay <146330062+TipJay@users.noreply.github.com>
2025-11-13 15:04:22 +0100

    Update universal-test.js

--- universal-test.js
+++ universal-test.js
@@ -1,6 +1,6 @@
 (function() {
   const s = document.createElement('script');
-  s.src = "https://dialogintelligens.github.io/scripts/universal-local.js?id=vinhuset";
+  s.src = "https://dialogintelligens.github.io/scripts/universal-local.js?id=skoringen";
   s.defer = true;
   document.head.appendChild(s);
 })();
