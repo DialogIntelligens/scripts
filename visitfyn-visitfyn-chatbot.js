8f0b349c40db241e89cc1c0d0139ab2ece27b52f
DialogIntelligens <145685451+DialogIntelligens@users.noreply.github.com>
2025-11-14 22:47:14 +0100

    Create visitfyn-visitfyn-chatbot.js

---
+++ visitfyn-visitfyn-chatbot.js
@@ -0,0 +1,6 @@
+(function() {
+  const s = document.createElement('script');
+  s.src = "https://dialogintelligens.github.io/scripts/universal-chatbot.js?id=visitfyn";
+  s.defer = true;
+  document.head.appendChild(s);
+})();
