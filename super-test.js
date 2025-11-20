8b116544ea965a8d07af2454583df2bdc7985eda
DialogIntelligens <145685451+DialogIntelligens@users.noreply.github.com>
2025-11-20 13:30:44 +0100

    Update super-test.js

--- super-test.js
+++ super-test.js
@@ -1,6 +1,6 @@
 (function() {
   const s = document.createElement('script');
-  s.src = "https://dialogintelligens.github.io/scripts/universal-chatbot.js?id=supertest";
+  s.src = "https://dialogintelligens.github.io/scripts/universal-chatbot.js?id=super-test";
   s.defer = true;
   document.head.appendChild(s);
 })();
