d91244d48ab2a100c9c32cee9569012b47adfaa8
reWach <wasimchoukair@gmail.com>
2025-11-20 11:12:35 +0100

    add prerelease test file

---
+++ prerelease-test.js
@@ -0,0 +1,6 @@
+(function() {
+  const s = document.createElement('script');
+  s.src = "https://dialogintelligens.github.io/scripts/prerelease.js?id=skoringen";
+  s.defer = true;
+  document.head.appendChild(s);
+})();
