72ad70893c3d1646c2fcc4faa45b84a9afcc5161
DialogIntelligens <145685451+DialogIntelligens@users.noreply.github.com>
2025-11-25 20:54:20 +0100

    Update bodylab.js

--- bodylab.js
+++ bodylab.js
@@ -1,6 +1,13 @@
+<script>
 (function() {
+  // block on this exact URL
+  if (window.location.pathname === "/shop/checkout.html") {
+    return; // do nothing
+  }
+
   const s = document.createElement('script');
   s.src = "https://dialogintelligens.github.io/scripts/universal-chatbot.js?id=bodylab";
   s.defer = true;
   document.head.appendChild(s);
 })();
+</script>
