9be85491315627922f3d8e9bd80a356f09078130
DialogIntelligens <145685451+DialogIntelligens@users.noreply.github.com>
2025-11-30 14:25:50 +0100

    Update trafikskolerne.js

--- trafikskolerne.js
+++ trafikskolerne.js
@@ -1,6 +1,1 @@
-(function() {
-  const s = document.createElement('script');
-  s.src = "https://dialogintelligens.github.io/scripts/universal-chatbot.js?id=trafikskolerne";
-  s.defer = true;
-  document.head.appendChild(s);
-})();
+
