655496e0a869e9c793f38cd6bc98b165257af6e6
DialogIntelligens <145685451+DialogIntelligens@users.noreply.github.com>
2025-12-01 09:28:57 +0100

    Update skoringen-skoringen-no-chatbot.js

--- skoringen-skoringen-no-chatbot.js
+++ skoringen-skoringen-no-chatbot.js
@@ -1,5 +1,15 @@
-(function() {
-  const s = document.createElement('script');
+(function () {
+  const blocked = [
+    "/shop-vouchers",
+    "/shop-crm",
+    "/shop-klikk-hent"
+  ];
+
+  if (blocked.some(path => window.location.href.includes(path))) {
+    return;
+  }
+
+  const s = document.createElement("script");
   s.src = "https://dialogintelligens.github.io/scripts/universal-chatbot.js?id=skoringen-no";
   s.defer = true;
   document.head.appendChild(s);
