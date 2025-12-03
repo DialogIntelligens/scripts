11b012885188392a2c9034614b505ad81226d305
reWach <wasimchoukair@gmail.com>
2025-12-03 14:57:23 +0100

    update prerelease-test to target dev

--- prerelease-test.js
+++ prerelease-test.js
@@ -1,6 +1,6 @@
 (function() {
   const s = document.createElement('script');
-  s.src = "https://dialogintelligens.github.io/scripts/prerelease.js?id=skoringen";
+  s.src = "https://scripts-dev.onrender.com/prerelease.js?id=super-test";
   s.defer = true;
   document.head.appendChild(s);
 })();
