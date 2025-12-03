a4678f180fdb5473f9a92eff5ed2d029d9f2af0a
reWach <wasimchoukair@gmail.com>
2025-12-03 15:00:28 +0100

    update id

--- prerelease-test.js
+++ prerelease-test.js
@@ -1,6 +1,6 @@
 (function() {
   const s = document.createElement('script');
-  s.src = "https://scripts-dev.onrender.com/prerelease.js?id=super-test";
+  s.src = "https://scripts-dev.onrender.com/prerelease.js?id=skoringen";
   s.defer = true;
   document.head.appendChild(s);
 })();
