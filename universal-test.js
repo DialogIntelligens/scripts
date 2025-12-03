bd76abd99f2928fef7f77077ece8f14627c5c5a4
DialogIntelligens <145685451+DialogIntelligens@users.noreply.github.com>
2025-12-03 14:18:17 +0100

    Update universal-test.js

--- universal-test.js
+++ universal-test.js
@@ -1,6 +1,6 @@
 (function() {
   const s = document.createElement('script');
-  s.src = "https://dialogintelligens.github.io/scripts/universal-local.js?id=ditur";
+  s.src = "https://dialogintelligens.github.io/scripts/universal-local.js?id=super-test";
   s.defer = true;
   document.head.appendChild(s);
 })();
