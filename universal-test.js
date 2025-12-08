16913025774587dccd43fc6848f9308025d23c5e
DialogIntelligens <145685451+DialogIntelligens@users.noreply.github.com>
2025-12-08 11:58:11 +0100

    Update universal-test.js

--- universal-test.js
+++ universal-test.js
@@ -1,6 +1,6 @@
 (function() {
   const s = document.createElement('script');
-  s.src = "https://dialogintelligens.github.io/scripts/universal-local.js?id=super-test";
+  s.src = "https://dialogintelligens.github.io/scripts/universal-local.js?id=dillingdk";
   s.defer = true;
   document.head.appendChild(s);
 })();
