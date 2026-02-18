0473d803db1f1f18bf68fdc05a6c0eaa6b8a5df7
DialogIntelligens <145685451+DialogIntelligens@users.noreply.github.com>
2026-02-18 19:46:45 +0100

    notification

--- universal-chatbot.js
+++ universal-chatbot.js
@@ -420,8 +420,8 @@
 /* Notification badge styles */
 .notification-badge {
   position: absolute;
-  top: -2px;
-  right: 6px;
+  top: -4px;
+  right: 0px;
   min-width: 20px;
   height: 20px;
   padding: 0 5px;
