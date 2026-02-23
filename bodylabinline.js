07a2eec26c83756684ebe9ca4589e933b78fa326
Oliver-Goth <Olivergoth02@gmail.com>
2026-02-23 10:14:29 +0100

    removed sandbox element from bodylabinline

--- bodylabinline.js
+++ bodylabinline.js
@@ -44,10 +44,6 @@
      iframe.id = 'chat-iframe-inline';
      iframe.src = IFRAME_URL;
      iframe.style.display = 'block';
-     iframe.setAttribute(
-      'sandbox',
-      'allow-scripts allow-same-origin allow-forms allow-popups allow-popups-to-escape-sandbox'
-     );
  
      container.appendChild(iframe);
      const placeholder = document.getElementById('chatbot-placeholder');
