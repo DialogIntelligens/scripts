42e329c6029efbc053f3b8665371f5f0ca68e8ec
OliverLyksted <144331612+OliverLyksted@users.noreply.github.com>
2026-06-01 10:04:10 +0200

    Update dilling-dillingnl-chatbot.js

--- dilling-dillingnl-chatbot.js
+++ dilling-dillingnl-chatbot.js
@@ -1,6 +1,6 @@
 (function() {
   const s = document.createElement('script');
-  s.src = "https://dialogintelligens.github.io/scripts/universal-chatbot.js?id=dillingnl";
+  s.src = "https://dialogintelligens.github.io/scripts/dilling-custom-script.js?id=dillingnl";
   s.defer = true;
   document.head.appendChild(s);
 })();
