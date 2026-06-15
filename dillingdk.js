ac393474187656ba1ac5e3498b2684eab284873f
OliverLyksted <144331612+OliverLyksted@users.noreply.github.com>
2026-06-15 13:33:49 +0200

    Update script source URL for dillingdk.js

--- dillingdk.js
+++ dillingdk.js
@@ -1,6 +1,6 @@
 (function() {
   const s = document.createElement('script');
-  s.src = "https://dialogintelligens.github.io/scripts/dilling-custom-script.js?id=dillingdk";
+  s.src = "https://dialogintelligens.github.io/scripts/universal-chatbot.js?id=dillingdk";
   s.defer = true;
   document.head.appendChild(s);
 })();
