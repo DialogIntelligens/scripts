81eff69b28641d53abaab3d7df023c80d47065d2
OliverLyksted <144331612+OliverLyksted@users.noreply.github.com>
2026-08-12 09:58:12 +0200

    Update chatbot script source URL

--- dilling-dillingde-chatbot.js
+++ dilling-dillingde-chatbot.js
@@ -1,6 +1,6 @@
 (function() {
   const s = document.createElement('script');
-  s.src = "https://dialogintelligens.github.io/scripts/universal-chatbot.js?id=dillingde";
+  s.src = "https://dialogintelligens.github.io/scripts/dilling-custom-script.js?id=dillingde";
   s.defer = true;
   document.head.appendChild(s);
 })();
